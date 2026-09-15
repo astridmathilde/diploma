ef35b074d89d9358ace6af45692efe9add4f4c66
astrid mathilde <heia@astridmathilde.no>
2026-09-15 12:46:09 +0200

    layout

--- src/app/[category]/[slug]/page.js
+++ src/app/[category]/[slug]/page.js
@@ -60,14 +60,11 @@ export default async function PostPage({params}) {
   
   return (
     <article>
-    <h1>{post.title}</h1>
+    <h2>{post.title}</h2>
     <PortableText value={post.content} components={components} />
     <footer>
      <p>Published at <time dateTime={post.publishedAt}>{formatDate(post.publishedAt)}</time>.<br />Last updated at <time dateTime={post._updatedAt}>{formatDate(post._updatedAt)}</time>.</p>
-        <p><em>Designing Calm: Tools for digital minimalism.</em> A diploma project by <a href="https://astridmathilde.no" rel="external" target="_blank">Astrid Mathilde Boberg</a>.</p>
-     <p><Link href="/">Back to index</Link></p>
-
-   
+     <p><Link href="/">Close</Link></p>
     </footer>
     </article>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -1,61 +1,6 @@
-import Link from 'next/link';
-
-import { formatDate } from '@/lib/date';
-import { getPosts } from '@/sanity/lib/data';
-
 export default async function Home() {
-  const posts = await getPosts()
-
-  const categories = []
-  const bySlug = new Map()
-  for (const post of posts) {
-    if (!bySlug.has(post.category.slug)) {
-      bySlug.set(post.category.slug, {
-        slug: post.category.slug,
-        name: post.category.name,
-        posts: [],
-      })
-      categories.push(bySlug.get(post.category.slug))
-    }
-    bySlug.get(post.category.slug).posts.push(post)
-  }
-
-  const lastUpdated = (category) =>
-    Math.max(...category.posts.map((post) => new Date(post._updatedAt ?? 0).getTime()))
-
-  categories.sort((a, b) => lastUpdated(b) - lastUpdated(a))
-
   return (
     <>
-      <h1>Designing calm: Tools for digital minimalism</h1>
-      <p>A diploma project by <a href="https://astridmathilde.no" rel="external" target="_blank">Astrid Mathilde Boberg</a>.<br />The Oslo School of Architecture and Design, Spring 2027.</p>
-
-      {categories.map((category) => (
-        <section key={category.slug}>
-          <h2>{category.name}</h2>
-          <ul>
-            {category.posts.map((post) => (
-              <li key={post._id}>
-                <Link href={`/${category.slug}/${post.slug}`}>{post.title}</Link>
-                {post.publishedAt ? (
-                  <>
-                  {' '}
-                  <time dateTime={post.publishedAt}>{formatDate(post.publishedAt)}</time>
-                  </>
-                ) : null}
-              </li>
-            ))}
-          </ul>
-        </section>
-      ))}
-
-      <section key="resources">
-        <h2>Resources</h2>
-        <ul>
-          <li><a href="https://www.are.na/astrid-mathilde-boberg/my-diploma-research" rel="external" target="_blank">Research</a></li>
-          <li><a href="https://www.are.na/astrid-mathilde-boberg/my-diploma-tools-methods" rel="external" target="_blank">Tools & methods</a></li>
-        </ul>
-      </section>
     </>
   )
 }
